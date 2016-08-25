756b05d4f303e6d1a9e6ae349e63f302d8a16e18
Alexis Thornton <alxs412@gmail.com>
2016-08-25 12:37:24 -0700

    takes input file and makes a z score output file. now has options

--- eVIP_corr.py
+++ eVIP_corr.py
@@ -1,20 +1,14 @@
-
 import sys
 import optparse
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import csv
-from scipy import stats
-import cmap.io.gct as gct
-import cmap.io.plategrp as grp
-import gp
+import os
+import pdb
 
+import rpy2.robjects as robjects
+from rpy2.robjects.packages import importr
 
 ###########
 # CLASSES #
 ###########
-
 class OptionParser(optparse.OptionParser):
     """
     Adding a method for required arguments.
@@ -25,10 +19,10 @@ class OptionParser(optparse.OptionParser):
         option = self.get_option(opt)
 
         # Assumes the option's 'default' is set to None!
-    #    if getattr(self.values, option.dest) is None:
-    #        print ("%s option not supplied") % option
-    #        self.print_help()
-    #        sys.exit(1)
+        if getattr(self.values, option.dest) is None:
+            print "%s option not supplied" % option
+            self.print_help()
+            sys.exit(1)
 
 ###############
 # END CLASSES #
@@ -42,72 +36,91 @@ def main():
     opt_parser = OptionParser()
 
     # Add Options. Required options should have default=None
-    opt_parser.add_option("--exp_table",
-                          dest="exp_table",
+    opt_parser.add_option("-i",
+                          dest="input",
                           type="string",
-                          help="""file with filtered and normalized gene expression data table""",
+                          help="""input txt file with filtered gene expression data""",
+                          default=None)
+    opt_parser.add_option("--z_out",
+                          dest="z_output",
+                          type="string",
+                          help="""name of output file containing z_scores""",
+                          default=None)
+    opt_parser.add_option("--sp_out",
+                          dest="sp_output",
+                          type="string",
+                          help="""name of output file containing spearman correlation""",
                           default=None)
 
-    # opt_parser.add_option("--output",
-    #                       dest="output",
-    #                       type="string",
-    #                       help="""name of output file """,
-    #                       default=None)
-
-
     (options, args) = opt_parser.parse_args()
+    opt_parser.check_required("-i")
+    opt_parser.check_required("--z_out")
+    opt_parser.check_required("--sp_out")
 
-    #validate command line arguments
-    #opt_parser.check_required("exp_table")
-    #opt_parser.check_required("output")
+    input_file = open(options.input)
+    z_output_file = open(options.z_output, "w")
+    spearman_output = open(options.sp_output, "w")
 
-    #exp_table_file= open(options.exp_table)
-    #output_file = open(options.output + ".gct","w")
+    outlines = []
+    mads = []
 
-    #turning expression table into matrix
-    #matrix = np.loadtxt(open("test.csv","rb"),delimiter=",",skiprows=1, usecols=[])
+    header = None
+    for line in input_file:
+        line = formatLine(line)
 
+        if line.startswith("#"):
+            header = line
+            z_output_file.write(header + "\n")
+            continue
 
+        lineList = line.split("\t")
 
-
-    matrix = np.loadtxt(open("test.csv","rb"),delimiter=",",skiprows=1, usecols=[1,2,3,4])
-
-
-
-
-    expmatrix_to_zscore(matrix)
-
-    print(matrix.shape)
-    print(matrix)
-
-    #exp_table.close()
-    sys.exit(0)
-
+        in_vals =[]
+        vals=[]
+        for item in lineList[1:]:
+            val = float(item)
+            in_vals.append(val)
+            vals.append(val)
 
 
+        median = robjects.r['mean'](robjects.FloatVector(vals))[0]
+        mad = robjects.r['sd'](robjects.FloatVector(vals))[0]
 
-############
-# END_MAIN #
-############
+        out_vals =[]
+
+        for v in in_vals:
+            new_val = (v-median)/mad
+            out_vals.append(new_val)
+
+        out_vals_str = []
+        for v in out_vals:
+            out_vals_str.append("%.4f" % v)
+
+        outline = "\t".join(lineList[:1]) + "\t"
+        outline += "\t".join(out_vals_str)
+        outline += "\n"
+
+        outlines.append(outline)
+        mads.append(mad)
+
+    for outline in outlines:
+        z_output_file.write(outline)
+
+    input_file.close()
+    z_output_file.close()
+    spearman_output.close()
 
 #############
 # FUNCTIONS #
 #############
-def expmatrix_to_zscore(matrix):
-    print(matrix)
-    #getting zscore matrix
-    zscore_mat = stats.zscore(matrix)
-    print(zscore_mat)
 
-    #spearman rank pairwise correlatin
-    spearm_mat = stats.spearmanr(zscore_mat)
-    print(spearm_mat)
-
-def create_gct(output):
-    
-
+def formatLine(line):
+    line = line.replace("\r","")
+    line = line.replace("\n","")
+    return line
 
 #################
 # END FUNCTIONS #
 #################
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
